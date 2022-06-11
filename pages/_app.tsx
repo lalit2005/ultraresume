3bc09258322c2ee03de71303fd4e62b414fc8d39
Lalit <lalitvijay9480@gmail.com>
2022-06-11 10:03:45 +0530

    feat: init

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/inter/variable.css';
+
+import theme from '../theme';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ theme.tsx
@@ -0,0 +1,22 @@
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+const breakpoints = createBreakpoints({
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  xl: "80em",
+});
+
+const theme = extendTheme({
+  colors: {
+    black: "#16161D",
+  },
+  fonts: {
+    heading: "InterVariable, monospace",
+    body: "InterVariable, Monospace",
+  },
+  breakpoints,
+});
+
+export default theme;

e69b332a6271710655e628b02b1e5b1007860ab4
Lalit <lalitvijay9480@gmail.com>
2022-06-12 13:12:21 +0530

    feat add demo links in landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import PrimaryLayout from '@/layouts/PrimaryLayout';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Link, Text } from '@chakra-ui/react';
 
 const IndexPage = () => {
   return (
@@ -16,6 +16,32 @@ const IndexPage = () => {
             PRO
           </Text>
         </Heading>
+        <Text fontSize='lg'>Build your resume in seconds not in minutes</Text>
+        <Button
+          mt='10'
+          as='a'
+          size='lg'
+          shadow='xl'
+          _hover={{
+            shadow: '2xl',
+          }}
+          rounded='full'
+          href='https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6941639638868488192?compact=1'
+          target='_blank'
+          rel='noopener noreferrer'>
+          View demo {'->'}
+        </Button>
+        <Text mt='10'>
+          Made by{' '}
+          <Link
+            color='twitter.500'
+            fontWeight='bold'
+            href='http://lalit.codes'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Lalit
+          </Link>
+        </Text>
       </Box>
     </PrimaryLayout>
   );
